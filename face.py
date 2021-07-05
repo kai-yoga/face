1ef6657dbce79c6ec1df2ccd24368dd6747fc036
kai-y <1532398723@qq.com>
2021-07-05 14:17:29 +0800

    --edit 20210705

--- face.py
+++ face.py
@@ -75,10 +75,8 @@ def getSimilar(lib_file, other_file):
     lib_encoding = face_recognition.face_encodings(lib_face)[0]
     other_face = face_recognition.load_image_file(other_file)
     other_encoding = face_recognition.face_encodings(other_face)[0]
-    similar = 1 - face_recognition.face_distance(
-        [lib_encoding],
-        other_encoding
-    )[0]
+    similar = 1 - face_recognition.face_distance([lib_encoding],
+        other_encoding)[0]
     return round(similar, 4)
 
 
@@ -131,28 +129,30 @@ def main(config):
     config = config["bdzp"]
     compare = config["compare"]
     libs = config["libs"]
-    res = [('比较', '基础', '相似度', '比较照片', '基础照片')]
-    for others in config["others"]:
-        to_convert = getToConvert(libs, others)
-        convert_file = None
-        if to_convert != (0, 0):
-            convert_file = getConvertFile(others["convert_file_path"])
-        for file in getOthers(others["path"]):
-            lib_file = getLibFileName(convert_file, file, to_convert)
-            other_file = os.path.join(others["path"], file)
-            similar = 0
-            if os.path.exists(os.path.join(libs["path"], lib_file)):  ###lib_file中不存在对应的文件
-                lib_file = os.path.join(libs["path"], lib_file)
-                if lib_file.split('.')[-1] in compare["suffix"] and other_file.split('.')[-1] in compare["suffix"]:
-                    similar = getSimilar(lib_file, other_file)
-            else:
-                lib_file = '不存在'
-                # similar = 0
-            if similar < float(others["similar"]):
-                res.append((other_file, lib_file, str(similar)))
-    res_file_name = os.path.join(compare["save_path"],
-                                 datetime.now().strftime('%Y-%m-%d %H%M%S') + '.' + compare["outputFileType"].lower())
-    expxlsx(res_file_name, res, compare["outputFileType"].lower())
+    if libs["path"] and libs["file_key"]:
+        res = [('比较', '基础', '相似度', '比较照片', '基础照片')]
+        for others in config["others"]:
+            if others["file_key"] and others["path"] and other["similar"]:
+                to_convert = getToConvert(libs, others)
+                convert_file = None
+                if to_convert != (0, 0):
+                    convert_file = getConvertFile(others["convert_file_path"])
+                for file in getOthers(others["path"]):
+                    lib_file = getLibFileName(convert_file, file, to_convert)
+                    other_file = os.path.join(others["path"], file)
+                    similar = 0
+                    if os.path.exists(os.path.join(libs["path"], lib_file)):  ###lib_file中不存在对应的文件
+                        lib_file = os.path.join(libs["path"], lib_file)
+                        if lib_file.split('.')[-1] in compare["suffix"] and other_file.split('.')[-1] in compare["suffix"]:
+                            similar = getSimilar(lib_file, other_file)
+                    else:
+                        lib_file = '不存在'
+                        # similar = 0
+                    if similar < float(others["similar"]):
+                        res.append((other_file, lib_file, str(similar)))
+            res_file_name = os.path.join(compare["save_path"],
+                                         datetime.now().strftime('%Y-%m-%d %H%M%S') + '.' + compare["outputFileType"].lower())
+            expxlsx(res_file_name, res, compare["outputFileType"].lower())
 
 
 # if __name__ == "__main__":
--- zipzp.py
+++ zipzp.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
-
+from datetime import datetime
 
 def getFiles(file_path, file_type_list):
     '''Generator
@@ -23,29 +23,21 @@ def genZip(file_path, save_path, max_count, max_storage):
         if file:
             global_count = global_count + 1
             if sumsize + file['size'] < max_storage * 1024 * 1024 and count < max_count:
-                zipfile.ZipFile(
-                    os.path.join(save_path, str(index) + '.zip'),
+                zipfile.ZipFile(os.path.join(save_path, str(index) + '.zip'),
                     mode='a',
-                    compression=zipfile.ZIP_BZIP2
-                ).write(file['name'])
+                    compression=zipfile.ZIP_BZIP2).write(file['name'])
                 count = count + 1
                 sumsize = sumsize + file['size']
             else:
-                L.append('{},已经生成完成，大小=>{},文件数量=>{}'.format(
-                    os.path.join(save_path, str(index) + '.zip'), str(round(sumsize / 1024)) + 'K', str(count)
-                ))
+                L.append('{},已经生成完成，大小=>{},文件数量=>{}'.format(os.path.join(save_path, str(index) + '.zip'), str(round(sumsize / 1024)) + 'K', str(count)))
                 index = index + 1
-                zipfile.ZipFile(
-                    os.path.join(save_path, str(index) + '.zip'),
+                zipfile.ZipFile(os.path.join(save_path, str(index) + '.zip'),
                     mode='a',
-                    compression=zipfile.ZIP_BZIP2
-                ).write(file['name'])
+                    compression=zipfile.ZIP_BZIP2).write(file['name'])
                 count = 1
                 sumsize = file['size']
     if os.path.exists(os.path.join(save_path, str(index) + '.zip')):
-        L.append('{},已经生成完成，大小=>{},文件数量=>{}'.format(
-            os.path.join(save_path, str(index) + '.zip'), str(round(sumsize / 1024)) + 'K', str(count)
-        ))
+        L.append('{},已经生成完成，大小=>{},文件数量=>{}'.format(os.path.join(save_path, str(index) + '.zip'), str(round(sumsize / 1024)) + 'K', str(count)))
     return L
 
 
@@ -58,12 +50,20 @@ def init(save_path):
 
 def main(config):
     '''主函数'''
-    config=config["zip"]
-    file_path=config["file_path"]
-    save_path=config["save_path"]
-    save_file_suffix=config["save_file_suffix"]
-    max_count=config["max_count"]
-    max_storage=config["max_storage"]
-    if init(save_path):
-        print('{}路径中的压缩文件已经清空完毕'.format(save_path))
+    config = config["zip"]
+    file_path = config["file_path"]
+    save_path = config["save_path"]
+    save_file_suffix = config["save_file_suffix"]
+    if file_path and save_path and save_file_suffix:
+        max_count = float(config["max_count"])
+        max_storage = float(config["max_storage"])
+        if init(save_path):
+            print('{}路径中的压缩文件已经清空完毕'.format(save_path))
+        L = genZip(file_path,save_path,max_count=max_count,max_storage=max_storage)
+        with open(os.path.join(save_path,'zip_res' + datetime.now().strftime('%Y-%m-%d %H%M%S') + '.txt'),'w',encoding='utf8') as f:
+            f.write('\n'.join(L))
+        f.close()
+
+
+
 
